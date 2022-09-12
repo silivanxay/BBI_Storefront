eb3b8af604a4824b81df5bc0fbb5925533b5c7c7
PernDev <Pern.dev@gmail.com>
2022-09-12 11:53:07 +0700

    Set data

--- components/layout/admin/FormInput.jsx
+++ components/layout/admin/FormInput.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { NumericFormat } from "react-number-format";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const initialState = {
+  title: "",
+  price: "",
+  subitle: "",
+  dessiption: "",
+};
 
 const FormInput = () => {
+  const [data, setData] = useState(initialState);
+
+  const handleChange = (e) => {
+    setData(
+      {
+        ...data,
+        [e.target.name]: e.target.value,
+      },
+      console.log(data)
+    );
+  };
+
+  const handleSubmit = () => {
+    console.log(data);
+    if (data.title === "") {
+      toast.error("Title can’t be blank");
+    } else if (data.price === "") {
+      toast.error("Price can’t be blank");
+    } else if (data.subitle === "") {
+      toast.error("Subitle can’t be blank");
+    } else if (data.dessiption === "") {
+      toast.error("Dessiption can’t be blank");
+    } else {
+      // toast.success("Success");
+      console.log(data);
+      // setNewProduct(data)
+      //   .then((res) => {
+      //     console.log(res.data);
+      //   })
+      //   .catch((err) => {
+      //     toast.error("cannot connect api");
+      //     console.log(err);
+      //   });
+    }
+  };
+
   return (
     <>
-      <div className="max-w-lg  bg-gray-900 shadow-2xl rounded-lg mx-auto text-center py-12 mt-4 ">
+      <div className="max-w-lg  bg-gray-900 shadow-2xl rounded-lg m-auto text-center py-12 mt-4 ">
         <h1 className="text-gray-200 text-center font-extrabold -mt-3 text-3xl">
           Add product
         </h1>
@@ -20,6 +67,31 @@ const FormInput = () => {
                 className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="title"
                 type="text"
+                name="title"
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
+
+            <div className="text-left">
+              <label for="price" className="text-white ">
+                Price
+              </label>
+            </div>
+            <div className="mb-4">
+              <NumericFormat
+                placeholder=" Price"
+                className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="title"
+                name="price"
+                thousandSeparator={false}
+                onValueChange={({ value }) => {
+                  setData({
+                    ...data,
+                    price: value,
+                  });
+                  console.log(value);
+                }}
+                maxLength={15}
               />
             </div>
 
@@ -34,6 +106,8 @@ const FormInput = () => {
                 className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="subitle"
                 type="text"
+                name="subitle"
+                onChange={(e) => handleChange(e)}
               />
             </div>
             <div className="text-left">
@@ -48,9 +122,10 @@ const FormInput = () => {
               ></label>
               <textarea
                 class="appearance-none w-full p-3 text-xs font-semibold leading-none bg-gray-50 rounded outline-none"
-                name="field-name"
+                name="dessiption"
                 rows="3"
                 placeholder="Write something..."
+                onChange={(e) => handleChange(e)}
               ></textarea>
             </div>
           </form>
@@ -100,6 +175,7 @@ const FormInput = () => {
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 type="button"
+                onClick={() => handleSubmit()}
               >
                 Save
               </button>
@@ -107,182 +183,7 @@ const FormInput = () => {
           </form>
         </div>
       </div>
-
-      {/* END */}
-
-      <div className="max-w-lg  bg-gray-900 shadow-2xl rounded-lg mx-auto text-center py-12 mt-4 ">
-        <h1 className="text-gray-200 text-center font-extrabold -mt-3 text-3xl">
-          Media
-        </h1>
-        <div className="container py-5 max-w-md mx-auto">
-          <>
-            <label
-              htmlFor="first_name"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              First name
-            </label>
-            <input
-              type="text"
-              id="first_name"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="John"
-              required=""
-            />
-            <div>
-              <label
-                htmlFor="last_name"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Last name
-              </label>
-              <input
-                type="text"
-                id="last_name"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Doe"
-                required=""
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="company"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Company
-              </label>
-              <input
-                type="text"
-                id="company"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Flowbite"
-                required=""
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="phone"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Phone number
-              </label>
-              <input
-                type="tel"
-                id="phone"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="123-45-678"
-                pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-                required=""
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="website"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Website URL
-              </label>
-              <input
-                type="url"
-                id="website"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="flowbite.com"
-                required=""
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="visitors"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Unique visitors (per month)
-              </label>
-              <input
-                type="number"
-                id="visitors"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder=""
-                required=""
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Email address
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="john.doe@company.com"
-                required=""
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="•••••••••"
-                required=""
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="confirm_password"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Confirm password
-              </label>
-              <input
-                type="password"
-                id="confirm_password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="•••••••••"
-                required=""
-              />
-            </div>
-            <div className="flex items-start mb-6">
-              <div className="flex items-center h-5">
-                <input
-                  id="remember"
-                  type="checkbox"
-                  defaultValue=""
-                  className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                  required=""
-                />
-              </div>
-              <label
-                htmlFor="remember"
-                className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-              >
-                I agree with the{" "}
-                <a
-                  href="#"
-                  className="text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  terms and conditions
-                </a>
-                .
-              </label>
-            </div>
-            <button
-              type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Submit
-            </button>
-          </>
-        </div>
-      </div>
+      <ToastContainer />
     </>
   );
 };
