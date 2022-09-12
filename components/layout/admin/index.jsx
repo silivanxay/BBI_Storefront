257c24c121fd55b64f8dd2b34759e934ded9ac43
PernDev <Pern.dev@gmail.com>
2022-09-12 10:28:32 +0700

    Form input product

--- components/layout/admin/index.jsx
+++ components/layout/admin/index.jsx
@@ -6,7 +6,7 @@ const index = () => {
   return (
     <>
       <div class="flex flex-wrap -mx-4 -mb-4 md:mb-0">
-        <div class="w-full md:w-1/3 px-4 mb-4 md:mb-0">
+        <div class="w-full md:w-1/3 px-4 mb-4 md:mb-0 fixed  top-0  h-screen">
           <Sidebar />
         </div>
         <div class="w-full md:w-2/3 px-4 mb-4 md:mb-0 mt-10">
