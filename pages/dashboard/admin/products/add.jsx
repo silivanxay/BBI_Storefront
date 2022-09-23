79753028d95fd9f8b335786ea57951b841cadaba
PernDev <Pern.dev@gmail.com>
2022-09-23 19:15:21 +0700

    upload

--- pages/dashboard/admin/products/add.jsx
+++ pages/dashboard/admin/products/add.jsx
@@ -1,41 +1,92 @@
 import axios from "axios";
+import { language } from "gray-matter";
 import React, { useState } from "react";
 // import { setNewProduct } from "./API/productApi";
 import { NumericFormat } from "react-number-format";
 import { toast } from "react-toastify";
 import AdminLayout from "../../../../components/layouts/admin";
+// import UploadPro from "./API/productApi"
 
+const transModel = {
+  title:"",
+  description:"",
+};
+
+const LanguageModel = {
+  language:"la",
+}
+
+const translations = {};
+translations[LanguageModel.language] = transModel;
 
 const initialState = {
-  price: "11111",
-  translations: { en: { title: "Nike Shoe", description: "Air Force" } },
-  user: 1,
+  // price: "100000",
+  // slug:"vansss",
+  // translations: translations,
+  // user: 1,
+  ...translations
 };
 
 const AddProduct = () => {
 
-  const [formData, setFormData] = useState(initialState);
-  const [tran, setTran] = useState({
-    en: { title: "Nike Shoe", description: "Air Force" },
-  });
-  const [file, setFile] = useState(null);
+  const upload = JSON.stringify(initialState);
+
+  const [trans, setTrans] = useState(transModel)
+
+  const [language, setLanguage] = useState(LanguageModel);
+
+  const [formData, setFormData] = useState(upload);
+
+  const [image, setImage] = useState(null);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleChangeTrans = (e) =>{
+    setTrans({...trans, [e.target.name]: e.target.value })
+  }
+
+  const handleChangeLanguage = (e) => {
+    setLanguage({ ...language, [e.target.name]: e.target.value });
   };
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
   };
 
-  const AddNewProduct = (product) => {
-    return axios.post("http://localhost:8000/api/v1/product/", product);
+  const AddNewProduct = () => {
+    // const tran = "{\"en\":{\"title\":\"NikeShoe\",\"description\":\"AirForce\"}}";
+    const trans1 = {
+      en: {
+        title: "TC",
+        description: "UN",
+      },
+    };
+
+    
+    const sentdata = {
+      translations: upload,
+      // translations:"{\"en\":{\"title\":\"NikeShoe\",\"description\":\"AirForce\"}}",
+      slug: "asdfghjkl",
+      price: "010101",
+      audience: "Public",
+      user: 1,
+    }
+    console.log (sentdata)
+
+    return axios({
+      method: "post",
+      url: "http://localhost:8000/api/v1/product/",
+      data: sentdata,
+      headers: { "Content-Type": "multipart/form-data" },
+    });
   };
 
-  const translate = { en: { title: "Nike Shoe", description: "Air Force" } };
+  const onFileChange = (e) => setImage(e.target.files[0]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-
     if (formData.title === "") {
       toast.error("Title can’t be blank");
     } else if (formData.price === "") {
@@ -59,12 +110,15 @@ const AddProduct = () => {
 
       // formFormData.append("image", file);
 
+      const formData = new FormData();
+      formData.append("image", image);
+
       AddNewProduct(formData, "token")
         .then((res) => {
           console.log(res.data);
         })
         .catch((err) => {
-          toast.error("cannot connect api");
+          // toast.error("cannot connect api");
           console.log(err);
         });
     }
@@ -72,14 +126,44 @@ const AddProduct = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit} encType="multipart/form-FormData">
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
+        <div className="max-w-lg  bg-gray-900 shadow-2xl rounded-lg mx-auto text-center py-12 mt-4 ">
+          <h1 className="text-gray-200 text-center font-extrabold mt-3 text-3xl">
+            Language
+          </h1>
+          <div className="container py-5 max-w-md mx-auto flex flex-col-2 justify-center">
+            <div className="bg-gray-200 text-lg  p-1 rounded-md">
+              <input
+              id="en"
+                type="radio"
+                name="language"
+                value="en"
+                onChange={handleChangeLanguage}
+                className="peer hidden"
+              />
+              <label htmlFor="en" className="block cursor-pointer select-none rounded-xl p-3 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">English</label>
+            </div>
+            <div className="bg-gray-200 ml-5 text-lg p-1 rounded-md">
+            <input
+                type="radio"
+                name="language"
+                value="la"
+                className="peer hidden"
+                id="la"
+                onChange={handleChangeLanguage}
+              />{" "}
+                 <label htmlFor="la" className="block cursor-pointer select-none rounded-xl p-3 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">Lao</label>
+            </div>
+          </div>
+        </div>
+
         <div className="max-w-lg  bg-gray-900 shadow-2xl rounded-lg m-auto text-center py-12 mt-4 ">
           <h1 className="text-gray-200 text-center font-extrabold -mt-3 text-3xl">
             Add product
           </h1>
           <div className="container py-5 max-w-md mx-auto">
             <div className="text-left">
-              <label for="title" className="text-white ">
+              <label htmlFor="title" className="text-white ">
                 Title
               </label>
             </div>
@@ -90,18 +174,33 @@ const AddProduct = () => {
                 id="title"
                 type="text"
                 name="title"
-                onChange={(e) => handleChange(e)}
+                onChange={(e) => handleChangeTrans(e)}
               />
             </div>
 
             <div className="text-left">
-              <label for="price" className="text-white ">
+              <label htmlFor="slug" className="text-white ">
+                Slug
+              </label>
+            </div>
+            <div className="mb-4">
+              <input
+                placeholder="Slug"
+                className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="slug"
+                type="text"
+                name="slug"
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
+            <div className="text-left">
+              <label htmlFor="price" className="text-white ">
                 Price
               </label>
             </div>
             <div className="mb-4">
               <NumericFormat
-                placeholder=" Price"
+                placeholder="Price"
                 className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="title"
                 name="price"
@@ -119,8 +218,8 @@ const AddProduct = () => {
             </div>
 
             <div className="text-left">
-              <label for="subitle" className="text-white ">
-                Subitle
+              <label htmlFor="subitle" className="text-white ">
+                Subtitle
               </label>
             </div>
             <div className="mb-4">
@@ -129,72 +228,48 @@ const AddProduct = () => {
                 className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="subitle"
                 type="text"
-                name="subitle"
+                name="subtitle"
                 onChange={(e) => handleChange(e)}
               />
             </div>
             <div className="text-left">
-              <label for="dessiption" className="text-white ">
-                Dessiption
+              <label htmlFor="description" className="text-white ">
+              Description
               </label>
             </div>
-            <div class="mb-6">
+            <div className="mb-6">
               <label
-                class="block text-gray-800 text-sm font-semibold mb-2"
-                for=""
+                className="block text-gray-800 text-sm font-semibold mb-2"
+                htmlFor=""
               ></label>
               <textarea
-                class="appearance-none w-full p-3 text-xs font-semibold leading-none bg-gray-50 rounded outline-none"
-                name="dessiption"
+                className="appearance-none w-full p-3 text-xs font-semibold leading-none bg-gray-50 rounded outline-none"
+                name="description"
                 rows="3"
                 placeholder="Write something..."
-                onChange={(e) => handleChange(e)}
+                onChange={(e) => handleChangeTrans(e)}
               ></textarea>
             </div>
           </div>
         </div>
+
         <div className="max-w-lg  bg-gray-900 shadow-2xl rounded-lg mx-auto text-center py-12 mt-4 ">
           <h1 className="text-gray-200 text-center font-extrabold -mt-3 text-3xl">
             Media
           </h1>
           <div className="container py-5 max-w-md mx-auto">
-            <div class="flex justify-center items-center w-full">
-              <label
-                for="dropzone-file"
-                class="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-              >
-                <div class="flex flex-col justify-center items-center pt-5 pb-6">
-                  <svg
-                    aria-hidden="true"
-                    class="mb-3 w-10 h-10 text-gray-400"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      strokeWdth="2"
-                      d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                    ></path>
-                  </svg>
-                  <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    <span class="font-semibold">Click to upload</span> or drag
-                    and drop
-                  </p>
-                  <p class="text-xs text-gray-500 dark:text-gray-400">
-                    SVG, PNG, JPG or GIF (MAX. 800x400px)
-                  </p>
-                </div>
-                <input
-                  id="dropzone-file"
-                  type="file"
-                  name="image"
-                  class="hidden"
-                  onChange={handleFileChange}
-                />
+            <div className="text-left">
+              <label className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300 " htmlFor="image">
+                Image
               </label>
+            </div>
+            <div className="flex justify-center  w-full">
+              <input
+                className="block w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                type="file"
+                name="image"
+                onChange={onFileChange}
+              />
             </div>
 
             <div className="flex items-center justify-between mt-5">
@@ -211,6 +286,8 @@ const AddProduct = () => {
     </>
   );
 };
+
+
 
 export default AddProduct;
 
